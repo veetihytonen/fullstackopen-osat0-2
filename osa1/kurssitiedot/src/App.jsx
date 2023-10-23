85259ebffaaaf804a2f9f7c9c1e6c91a2b6c606e
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-10-23 18:13:51 +0300

    did exercises up to 1.2

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => (
+  <>
+    <h1>{props.course}</h1>
+  </>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+)
+
+const Content = (props) => (
+  <div>
+    <Part part={props.p1} exercises={props.e1} />
+    <Part part={props.p2} exercises={props.e2} />
+    <Part part={props.p3} exercises={props.e3} />
+  </div>
+)
+
+const Total = (props) => (
+  <>
+    <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+  </>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} p3={part3} e3={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+    </div>
+  )
+}
+
+export default App
