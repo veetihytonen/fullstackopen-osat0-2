eaea5f75b1af1ea17d5049f9faa70a2def8c3de8
Veeti Hytönen <veeti.hytonen@gmail.com>
2023-11-22 15:36:21 +0200

    did exercises up to to 2.7

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,24 +1,50 @@
 import { useState } from 'react'
 
+const Person = ({ person }) => (
+  <p>{person.name}</p>
+)
+
+const Persons = ({ persons }) => {
+  const asComponents = persons.map(person => <Person person={person} key={person.name} />)
+
+  return (
+    <>
+      {asComponents}
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  
+  const onNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
+  const onPersonSubmit = (event) => {
+    event.preventDefault()
+
+    const newPerson = { name: newName }
+
+    setPersons(persons.concat(newPerson))
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={onPersonSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={onNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <Persons persons={persons} />
     </div>
   )
 
